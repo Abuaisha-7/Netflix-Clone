5705ff19734169d3c819a30ee5a15e8777b3cbe2
Abuaisha-7 <heyrubedru8086@gmail.com>
2024-02-10 13:59:43 -1200

    header and footer added

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
 
   return (
     <>
-      <h1>Hellow</h1>
+      <Home/>
     </>
   )
 }
